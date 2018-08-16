36b10839ef3a67c6e2efe0affd7f2bac334b7476
fuzzygwalchmei <marc.falzon@gmail.com>
2018-08-16 16:45:46 +1000

    Added grid layout

--- Base_Corporation_App.py
+++ Base_Corporation_App.py
@@ -1,16 +1,34 @@
 from kivy.app import App
 from kivy.uix.spinner import Spinner
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
+from random import randint
+from NPC_generator import *
+from kivy.properties import NumericProperty, StringProperty, BooleanProperty,\
+    ListProperty
 
 class CorporationScreen(Screen):
+
     pass
 
-
-
 class CorporationApp(App):
+    NPC_List = ListProperty([])
+    def Add_NPC(self):
+        listLength = len(self.NPC_List)
+        newNPC = NPC_item(listLength)
+        self.NPC_List.append(newNPC)
+
+        #for attr,value in newNPC.__dict__.items():
+        #    print(attr,": ",value)
+
+        for each in self.NPC_List:
+            for attr,value in each.__dict__.items():
+                print(attr,": ",value, )
+
     def build(self):
         return CorporationScreen()
-        
+
 
 if __name__ == '__main__':
     CorporationApp().run()
--- NPC_generator.py
+++ NPC_generator.py
@@ -1,11 +1,17 @@
 from random import randint
 
 class NPC_item(object):
-    def __init__(self):
+
+    def __init__(self,myVal=0):
+        self.myRef = "NPC#: "+str(myVal)
         self.Action_Total = randint(9,13)
         self.Close_Combat = randint(9,13)
         self.Defence = randint(4,5)
         self.Hit_Points = randint(20,30)
+
+    def Number_Generator(self):
+        for each in range(1,1000):
+            yield each
 
 newItem = NPC_item()
 
